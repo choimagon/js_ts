966678a697b5c060100b8b600a948bd175eedede
choejihun <magon8696@naver.com>
2022-09-30 13:30:15 +0900

    js1일차

---
+++ test/app.js
@@ -0,0 +1,16 @@
+let a = 5;
+const b = 3;
+//var 이라는것이 있음 요즘은 보통 let const 사용 
+//var 은 아무규칙없이 수정 가능
+
+console.log("sum : " + (a+b));
+
+a = 10;
+
+console.log("new sum : " + (a+b));
+//------------------------------------
+console.log("---------------------------");
+const tf = false; //null은 아무것도 없다라는 뜻임
+console.log(tf);
+let something; //정의 되지않음 null과는 다름 null은 인위적으로 없는거임
+console.log(something);
