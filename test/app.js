ff97ded970162c9a1923505009d76bf966081fea
choejihun <magon8696@naver.com>
2022-09-30 14:00:53 +0900

    2번째1

--- test/app.js
+++ test/app.js
@@ -1,21 +1,22 @@
-let a = 5;
-const b = 3;
-//var 이라는것이 있음 요즘은 보통 let const 사용 
-//var 은 아무규칙없이 수정 가능
-
-console.log("sum : " + (a+b));
-a = 10;
-console.log("new sum : " + (a+b));
-//------------------------------------
-console.log("---------------------------");
-const tf = false; //null은 아무것도 없다라는 뜻임
-console.log(tf);
-let something; //정의 되지않음 null과는 다름 null은 인위적으로 없는거임
-console.log(something);
 //------------------------------------
 console.log("---------------------------");
 const dw = [1,2,3,4,5];
 console.log(dw);
 console.log(dw[0]);
 console.log(dw.length);
-//
+dw.push(6);
+console.log(dw);
+
+//const player = ["magon", 22, false,"bigger"];
+const player = { //object?
+    name : "magon",
+    points : 10,
+    fat : true,
+};
+console.log(player.name);
+console.log(player.points);
+console.log(player.fat);
+console.log(player);
+player.fat = false; // player 자체는 불변이지만 안에 것들은 가변이다
+player.lastname = "choi"; //안에 없는것을 추가 할수 있다. 
+console.log(player);
