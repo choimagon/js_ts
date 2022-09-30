d3f3d9d2ebf86af259e421f74107c7f211384d8e
choejihun <magon8696@naver.com>
2022-09-30 13:39:09 +0900

    첫푸시

--- test/app.js
+++ test/app.js
@@ -4,9 +4,7 @@ const b = 3;
 //var 은 아무규칙없이 수정 가능
 
 console.log("sum : " + (a+b));
-
 a = 10;
-
 console.log("new sum : " + (a+b));
 //------------------------------------
 console.log("---------------------------");
@@ -14,3 +12,9 @@ const tf = false; //null은 아무것도 없다라는 뜻임
 console.log(tf);
 let something; //정의 되지않음 null과는 다름 null은 인위적으로 없는거임
 console.log(something);
+//------------------------------------
+console.log("---------------------------");
+const dw = [1,2,3,4,5];
+console.log(dw);
+console.log(dw[0]);
+console.log(dw.length);
