d768a7e8277bb915371c4f0490e0e643abadc716
choejihun <magon8696@naver.com>
2022-09-30 22:56:18 +0900

    로그인 기능

--- test/app.js
+++ test/app.js
@@ -1,16 +1,29 @@
-//------------------------------------
-function sayHello(name, age){
-    console.log("My name is "+name+" and I'm " + age);
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const LOCALSTORAGE_USERNAME = "username";
+
+function loginSubmit(event){
+    event.preventDefault(); //브라우저의 기본 동작을 막는 것
+    const username = loginInput.value;
+    loginForm.className = HIDDEN_CLASSNAME;
+    localStorage.setItem(LOCALSTORAGE_USERNAME, username);
+    paintGreeting(username);
 }
-sayHello("magon", 22);
-function plus(a, b){
-    console.log("plus : " + (a+b));
+
+function paintGreeting(username){
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
 }
-plus(10, 20);
-const player = {
-    n : "dragon",
-    f : function(){
-        console.log("name : " +this.n); //this는 자바나 자바스크립트나 똑같은거 같다.
-    },
-};
-player.f();
+
+const saveUsername = localStorage.getItem(LOCALSTORAGE_USERNAME);
+
+if(saveUsername == null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", loginSubmit);
+} else {
+    paintGreeting(saveUsername);
+}
